8989ce180c36fc18c6af93940eb69ed7021e363b
Nuno Costa <n.r.t.c@hotmail.com>
2021-05-03 13:07:16 +0100

    Add main file

---
+++ cmake-build-debug/CMakeFiles/feup_cal_proj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/feup_cal_proj.dir/main.cpp.obj"
+  "feup_cal_proj.exe"
+  "feup_cal_proj.exe.manifest"
+  "feup_cal_proj.pdb"
+  "libfeup_cal_proj.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/feup_cal_proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by nrtc on 03/05/2021.
+//
+
+int main(){
+    return 0;
+}
